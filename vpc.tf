ee8512ddd0d32bc6d5c66711e0e7317095fd7d46
Kapavarapu <durgadevi.kapavarapu@accenture.com>
2025-07-15 00:47:22 +0530

    commiting module test

--- vpc.tf
+++ vpc.tf
@@ -4,4 +4,7 @@ module "vpc"{
     environment = var.environment
     vpc_cidr = var.vpc_cidr
     common_tags = var.common_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
 }
